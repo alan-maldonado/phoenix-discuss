d6e9711b7897c1e408ace28927cbaa1508f60ed7
alan-maldonado <alan.maldonado@wizeline.com>
2017-03-11 23:52:38 -0600

    list of topics, remove page controller

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,6 +2,12 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
