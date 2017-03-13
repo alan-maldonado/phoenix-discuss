cee9802d03076919d2142bc0a42ce7b4d6f199e6
alan-maldonado <alan.maldonado@wizeline.com>
2017-03-13 00:02:20 -0600

    secure postgres

--- config/dev.exs
+++ config/dev.exs
@@ -33,11 +33,6 @@ config :logger, :console, format: "[$level] $message\n"
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# Configure your database
-config :discuss, Discuss.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "alan",
-  password: "",
-  database: "discuss_dev",
-  hostname: "localhost",
-  pool_size: 10
+# Finally import the config/prod.secret.exs
+# which should be versioned separately.
+import_config "dev.secret.exs"
